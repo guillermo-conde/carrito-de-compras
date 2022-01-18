fbee328bc35494cd6c6849f5fc4469264c46f935
Guillermo Conde Flores <condeguillermo8@gmail.com>
2022-01-18 11:19:59 -0600

    Estado inicial de la app y carga de imagenes de productos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    productos:[
+      {name: 'Tomate', precio: 15.00, img: '/productos/tomate.jpg'},
+      {name: 'Arbejas', precio: 25.00, img: '/productos/arbejas.jpg'},
+      {name: 'Lechuga', precio: 7.00, img: '/productos/lechuga.jpg'}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <p>Hola Mundo</p>
+      </div>
+    );
+  };
 }
 
 export default App;
